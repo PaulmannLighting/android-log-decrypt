4b176afbf5acd809280518eba1a7457a9ecc6920
Richard Neumann <neumann@paulmann.de>
2023-10-10 14:20:19 +0200

    Use logging

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use android_log_decrypt::decrypt;
 use clap::Parser;
+use log::error;
 use std::fs::read_to_string;
 use std::io::{stdout, Write};
 use std::path::PathBuf;
@@ -14,21 +15,23 @@ struct Args {
 }
 
 fn main() {
+    env_logger::init();
+
     let args = Args::parse();
     let ciphertext = read_to_string(&args.filename).unwrap_or_else(|_| {
-        eprintln!("Could not read file: {:?}", args.filename);
+        error!("Could not read file: {:?}", args.filename);
         exit(1);
     });
     let password = hex::decode(&args.key).unwrap_or_else(|_| {
-        eprintln!("Invalid hex key: {}", args.key);
+        error!("Invalid hex key: {}", args.key);
         exit(2);
     });
     let plain_text = decrypt(&ciphertext, &password).unwrap_or_else(|error| {
-        eprintln!("{error}");
+        error!("{error}");
         exit(3);
     });
     stdout().write_all(&plain_text).unwrap_or_else(|error| {
-        eprintln!("{error}");
+        error!("{error}");
         exit(4);
     });
 }
