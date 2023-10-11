55664906966e3a378e6fbd1d3f08f8d50fdb3f92
Richard Neumann <neumann@paulmann.de>
2023-10-11 10:00:23 +0200

    Remove single-use functions

--- src/encrypted_log.rs
+++ src/encrypted_log.rs
@@ -4,12 +4,11 @@ use aes::cipher::block_padding::Pkcs7;
 use aes::cipher::KeyIvInit;
 use aes::Aes256;
 use anyhow::anyhow;
-use cbc::cipher::{BlockDecryptMut, InvalidLength};
+use cbc::cipher::BlockDecryptMut;
 use cbc::Decryptor;
 use header::{Header, HEADER_HEX_LEN};
 use hex::{FromHex, ToHex};
-use hmac::digest::core_api::CoreWrapper;
-use hmac::{Hmac, HmacCore, Mac};
+use hmac::{Hmac, Mac};
 use sha2::Sha256;
 
 /// An encrypted log file
@@ -43,27 +42,21 @@ impl EncryptedLog {
     /// # Errors
     /// Returns an [`anyhow::Error`] on errors.
     pub fn decrypt(mut self, key: &[u8]) -> anyhow::Result<Vec<u8>> {
-        Ok(self
-            .cipher(key)
-            .decrypt_padded_mut::<Pkcs7>(&mut self.ciphertext)
-            .map_err(|error| anyhow!("{error}"))?
-            .to_vec())
+        Ok(
+            Decryptor::<Aes256>::new(key.into(), self.header.iv().into())
+                .decrypt_padded_mut::<Pkcs7>(&mut self.ciphertext)
+                .map_err(|error| anyhow!("{error}"))?
+                .to_vec(),
+        )
     }
 
     fn calculate_hmac(&self, key: &[u8]) -> anyhow::Result<Vec<u8>> {
-        let mut mac = self.hmac()?;
+        let mut mac =
+            Hmac::<Sha256>::new_from_slice(self.header.key().encode_hex::<String>().as_bytes())?;
         mac.update(self.header.iv().encode_hex::<String>().as_bytes());
         mac.update(self.ciphertext.encode_hex::<String>().as_bytes());
         mac.update(key.encode_hex::<String>().as_bytes());
         Ok(mac.finalize().into_bytes().to_vec())
-    }
-
-    fn hmac(&self) -> Result<CoreWrapper<HmacCore<Sha256>>, InvalidLength> {
-        Hmac::<Sha256>::new_from_slice(hex::encode(self.header.key()).as_bytes())
-    }
-
-    fn cipher(&self, key: &[u8]) -> Decryptor<Aes256> {
-        Decryptor::<Aes256>::new(key.into(), self.header.iv().into())
     }
 }
 
