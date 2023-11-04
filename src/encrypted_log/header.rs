bc779fd5381df877d82fd4fbb3e4e90c618ba4fa
Richard Neumann <neumann@paulmann.de>
2023-11-05 00:53:21 +0100

    Move magic numbers into constants.

--- src/encrypted_log/header.rs
+++ src/encrypted_log/header.rs
@@ -1,30 +1,33 @@
-pub const SIZE: usize = 64;
+const IV_SIZE: usize = 16;
+const KEY_SIZE: usize = 16;
+const HMAC_SIZE: usize = 32;
+pub const SIZE: usize = IV_SIZE + KEY_SIZE + HMAC_SIZE;
 
 #[derive(Debug, Eq, PartialEq)]
 pub struct Header {
-    iv: [u8; 16],
-    key: [u8; 16],
-    hmac: [u8; 32],
+    iv: [u8; IV_SIZE],
+    key: [u8; KEY_SIZE],
+    hmac: [u8; HMAC_SIZE],
 }
 
 impl Header {
     #[must_use]
-    pub const fn new(iv: [u8; 16], key: [u8; 16], hmac: [u8; 32]) -> Self {
+    pub const fn new(iv: [u8; IV_SIZE], key: [u8; KEY_SIZE], hmac: [u8; HMAC_SIZE]) -> Self {
         Self { iv, key, hmac }
     }
 
     #[must_use]
-    pub const fn iv(&self) -> &[u8; 16] {
+    pub const fn iv(&self) -> &[u8; IV_SIZE] {
         &self.iv
     }
 
     #[must_use]
-    pub const fn key(&self) -> &[u8; 16] {
+    pub const fn key(&self) -> &[u8; KEY_SIZE] {
         &self.key
     }
 
     #[must_use]
-    pub const fn hmac(&self) -> &[u8; 32] {
+    pub const fn hmac(&self) -> &[u8; HMAC_SIZE] {
         &self.hmac
     }
 }
@@ -33,9 +36,9 @@ impl Header {
 impl From<[u8; SIZE]> for Header {
     fn from(bytes: [u8; SIZE]) -> Self {
         Self::new(
-            bytes[0..16].try_into().unwrap(),
-            bytes[16..32].try_into().unwrap(),
-            bytes[32..SIZE].try_into().unwrap(),
+            bytes[0..IV_SIZE].try_into().unwrap(),
+            bytes[IV_SIZE..IV_SIZE + KEY_SIZE].try_into().unwrap(),
+            bytes[IV_SIZE + KEY_SIZE..SIZE].try_into().unwrap(),
         )
     }
 }
