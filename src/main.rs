b4a4028ad3a1e8cebef232646fc5ca723327f706
Richard Neumann <neumann@paulmann.de>
2023-12-07 12:52:50 +0100

    Move key reading to Args struct

--- src/main.rs
+++ src/main.rs
@@ -14,22 +14,31 @@ struct Args {
     key: Option<String>,
 }
 
+impl Args {
+    #[must_use]
+    pub fn hex_key(&self) -> String {
+        self.key.clone().unwrap_or_else(|| {
+            prompt_password("Decryption key: ").unwrap_or_else(|error| {
+                error!("{error}");
+                exit(1)
+            })
+        })
+    }
+
+    pub fn key(&self) -> Vec<u8> {
+        hex::decode(self.hex_key()).unwrap_or_else(|error| {
+            error!("{error}");
+            exit(2);
+        })
+    }
+}
+
 fn main() {
     env_logger::init();
 
     let args = Args::parse();
     let ciphertext = args.logfile.to_string();
-    let key = hex::decode(args.key.unwrap_or_else(|| {
-        prompt_password("Decryption key: ").unwrap_or_else(|error| {
-            error!("{error}");
-            exit(1)
-        })
-    }))
-    .unwrap_or_else(|error| {
-        error!("{error}");
-        exit(2);
-    });
-    let plain_text = decrypt(&ciphertext, &key).unwrap_or_else(|error| {
+    let plain_text = decrypt(&ciphertext, &args.key()).unwrap_or_else(|error| {
         error!("{error}");
         exit(3);
     });
